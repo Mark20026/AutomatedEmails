6ef273e54963951ea2cee658724572c64b3f4960
Mark20026 <bogimark2002@gmail.com>
2023-09-04 15:36:00 +0200

    Implementing the NewsFeed class

--- main.py
+++ main.py
@@ -4,5 +4,30 @@ from pprint import pprint
 
 
 class NewsFeed:
-    pass
+
+    def __init__(self, interest, language, from_param, to_param):
+        self.newsapi = NewsApiClient(api_key='d10d0741ce434ed2a1040cf447b1d41d')
+        self.interest = interest
+        self.language = language
+        self.from_param = from_param
+        self.to_param = to_param
+
+    def get(self):
+        headlines = self.newsapi.get_everything(qintitle=self.interest,
+                                                from_param=self.from_param,
+                                                to=self.to_param,
+                                                language=self.language)
+
+        articles = headlines["articles"]
+        email_body = ""
+
+        for article in articles:
+            email_body = email_body + article["title"] + "\n" + article["url"] + "\n\n"
+
+        return email_body
+
+
+news_file = NewsFeed("gym", "en", "2023-09-02", "2023-09-04")
+results = news_file.get()
+print(results)
 
