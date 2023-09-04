dfd5d3bdc51d30171a465ccf0980e48338219ab1
Mark20026 <bogimark2002@gmail.com>
2023-09-04 21:24:46 +0200

    Commented the whole code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 import yagmail
 from news import NewsFeed
 import pandas
@@ -5,15 +6,18 @@ import datetime
 import time
 
 
+# Define a function to send emails
 def send_email():
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    news_feed = NewsFeed(interest=row["interest"],
-                         from_param=yesterday,
-                         to_param=today)
-    # Type your email and app password here
-    sender = yagmail.SMTP(user="youremail@gmail.com",
-                          password="yourpassword")
+
+    # Create a NewsFeed object for the specified interest
+    news_feed = NewsFeed(interest=row["interest"], from_param=yesterday, to_param=today)
+
+    # Configure your email sender (replace with your actual email and password)
+    sender = yagmail.SMTP(user="youremail@gmail.com", password="your-password")
+
+    # Compose and send the email
     sender.send(to=row["email"],
                 subject=f"Your {row['interest']} news for today!",
                 contents=f"Hi {row['name']} \n"
@@ -21,11 +25,16 @@ def send_email():
                          f"{news_feed.get()}")
 
 
+# Continuously run the code
 while True:
+    # Check if the current time is 20:49
     if datetime.datetime.now().hour == 20 and datetime.datetime.now().minute == 49:
+        # Read the data from the "people.csv" file
         df = pandas.read_csv("people.csv")
 
+        # Iterate through the data and send emails to each person
         for index, row in df.iterrows():
             send_email()
 
+    # Wait for 60 seconds before checking the time again
     time.sleep(60)
--- news.py
+++ news.py
@@ -1,36 +1,47 @@
+# Import the necessary module for accessing news data
 from newsapi import NewsApiClient
 
 
+# Define a class called NewsFeed to retrieve news articles
 class NewsFeed:
 
+    # Constructor to initialize the class with required parameters
     def __init__(self, interest, from_param, to_param, language="en"):
+        # Initialize NewsApiClient with your API key (replace 'your_api_key' with your actual API key)
         self.newsapi = NewsApiClient(api_key='your_api_key')
-        self.interest = interest
-        self.language = language
-        self.from_param = from_param
-        self.to_param = to_param
+        self.interest = interest  # The topic or interest for which news is retrieved
+        self.language = language  # Language for the news (default is English)
+        self.from_param = from_param  # Start date for news retrieval
+        self.to_param = to_param  # End date for news retrieval
 
+    # Method to retrieve and format news articles
     def get(self):
-        headlines = self._get_headlines()
+        headlines = self._get_headlines()  # Call the private method to get news data
 
-        articles = headlines["articles"]
-        email_body = ""
+        articles = headlines["articles"]  # Extract the articles from the retrieved data
+        email_body = ""  # Initialize an empty string to store the email content
 
+        # Iterate through each article and append its title and URL to the email_body
         for article in articles:
             email_body = email_body + article["title"] + "\n" + article["url"] + "\n\n"
 
-        return email_body
+        return email_body  # Return the formatted email content
 
+    # Private method to fetch news headlines
     def _get_headlines(self):
+        # Use the NewsApiClient to retrieve news articles based on specified parameters
         headlines = self.newsapi.get_everything(qintitle=self.interest,
                                                 from_param=self.from_param,
                                                 to=self.to_param,
                                                 language=self.language)
-        return headlines
+        return headlines  # Return the retrieved headlines
 
 
+# Main section to demonstrate the NewsFeed class
 if __name__ == "__main__":
+    # Create a NewsFeed instance with specific parameters (interest, language, from_param, to_param)
     news_file = NewsFeed("gym", "en", "2023-09-02", "2023-09-04")
+
+    # Retrieve and print the formatted news content
     results = news_file.get()
     print(results)
-
